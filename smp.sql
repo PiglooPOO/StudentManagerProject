076e0545325faeba08e1f823839c79e7cf17e30a
Xavier GERBER <fmignon@etudiant.univ-mlv.fr>
2014-01-03 23:41:16 +0100

    Correction caractere bdd

--- smp.sql
+++ smp.sql
@@ -1,20 +1,24 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.4
+-- version 3.5.1
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Ven 03 Janvier 2014 à 20:32
--- Version du serveur: 5.6.12-log
--- Version de PHP: 5.4.12
+-- Généré le: Ven 03 Janvier 2014 à 22:39
+-- Version du serveur: 5.5.24-log
+-- Version de PHP: 5.3.13
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données: `smp`
 --
-CREATE DATABASE IF NOT EXISTS `smp` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `smp`;
 
 -- --------------------------------------------------------
 
@@ -24,10 +28,10 @@ USE `smp`;
 
 CREATE TABLE IF NOT EXISTS `formation` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) CHARACTER SET utf8 NOT NULL,
-  `diploma` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `diploma` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Contenu de la table `formation`
@@ -44,15 +48,15 @@ INSERT INTO `formation` (`id`, `name`, `diploma`) VALUES
 
 CREATE TABLE IF NOT EXISTS `student` (
   `number` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) CHARACTER SET utf8 NOT NULL,
-  `firstName` varchar(30) CHARACTER SET utf8 NOT NULL,
-  `adress` varchar(100) CHARACTER SET utf8 NOT NULL,
-  `phoneNumber` varchar(10) CHARACTER SET utf8 NOT NULL,
-  `mail` varchar(50) CHARACTER SET utf8 NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `firstName` varchar(30) NOT NULL,
+  `adress` varchar(100) NOT NULL,
+  `phoneNumber` varchar(10) NOT NULL,
+  `mail` varchar(50) NOT NULL,
   `birthday` date NOT NULL,
   `gender` tinyint(4) NOT NULL,
   PRIMARY KEY (`number`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Contenu de la table `student`
@@ -69,10 +73,10 @@ INSERT INTO `student` (`number`, `name`, `firstName`, `adress`, `phoneNumber`, `
 
 CREATE TABLE IF NOT EXISTS `subject` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL,
+  `name` varchar(30) CHARACTER SET latin1 NOT NULL,
   `coefficient` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `subject`
@@ -92,7 +96,7 @@ CREATE TABLE IF NOT EXISTS `year_formation_student` (
   `year` int(11) NOT NULL,
   `idFormation` int(11) NOT NULL,
   `idStudent` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `year_formation_student`
@@ -111,7 +115,7 @@ CREATE TABLE IF NOT EXISTS `year_formation_subject` (
   `year` int(11) NOT NULL,
   `idFormation` int(11) NOT NULL,
   `idSubject` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `year_formation_subject`
@@ -120,3 +124,7 @@ CREATE TABLE IF NOT EXISTS `year_formation_subject` (
 INSERT INTO `year_formation_subject` (`year`, `idFormation`, `idSubject`) VALUES
 (2013, 1, 1),
 (2013, 1, 2);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
