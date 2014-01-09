cc82ab2c92eee99eb098171943b85bc5304ae56b
Xavier GERBER <fmignon@etudiant.univ-mlv.fr>
2014-01-09 23:28:45 +0100

    Dernier COMMIT retirer gitignore

--- EclipseProject/deleteTables.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM student;
-DELETE FROM formation;
-DELETE FROM settings;
-DELETE FROM subject;
-DELETE FROM year_formation_student;
-DELETE FROM year_formation_subject;
-DELETE FROM year_student_subject_note;
